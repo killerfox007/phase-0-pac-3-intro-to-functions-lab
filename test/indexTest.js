cbcd478a0bb6a333e2b8a5eb29fd23ee61aec3a2
Kaleb Fox <kalebfox7@yahoo.com>
2024-04-30 18:30:42 -0400

    completed work

--- test/indexTest.js
+++ test/indexTest.js
@@ -52,3 +52,37 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
     );
   });
 })
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function loveTo(string) {
+ return "Let's have dinner together!"
+}
+
+function logShout(string){
+console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
+
+
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === whisper(string)) {
+     return "I can't hear you!";
+    } 
+    
+    else if (string === shout(string)){
+      return "YES INDEED!"
+    
+    } 
+    
+    else if (string === loveTo(string))
+    return 'I would love to!'
+  }
+    
+
+  
